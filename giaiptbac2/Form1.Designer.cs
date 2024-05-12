8629560b0a8e545fa7a2fe111084c2f5c6aed7a9
TienDung2111 <11tiendung21@gmail.com>
2024-05-12 23:39:48 +0700

    Add project files.

---
+++ giaiptbac2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace giaiptbac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            button1.BackColor = SystemColors.HotTrack;
+            button1.Font = new Font("Segoe UI", 10F);
+            button1.Location = new Point(50, 298);
+            button1.Name = "button1";
+            button1.Size = new Size(243, 71);
+            button1.TabIndex = 0;
+            button1.Text = "Giải phương trình";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += Button_click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 97);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(118, 147);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(118, 197);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(50, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(410, 28);
+            label1.TabIndex = 4;
+            label1.Text = "Nhập giá trị của hệ số của phương trình bậc 2";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(50, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Hệ số a:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(50, 150);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Hệ số b:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(50, 200);
+            label4.Name = "label4";
+            label4.Size = new Size(61, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Hệ số c:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.ErrorImage = (Image)resources.GetObject("pictureBox1.ErrorImage");
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(567, 40);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(266, 464);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(873, 530);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ giaiptbac2/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace giaiptbac2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_click(object sender, EventArgs e)
+        {
+            double a, b, c;
+
+            // Lấy giá trị từ các ô textbox
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+
+            // Sử dụng textBox3 thay vì lấy giá trị từ textBox3.Text
+            c = double.Parse(textBox3.Text);
+
+            double delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                MessageBox.Show($"Phương trình có 2 nghiệm phân biệt:\nx1 = {x1}\nx2 = {x2}");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                MessageBox.Show($"Phương trình có nghiệm kép:\nx = {x}");
+            }
+            else
+            {
+                MessageBox.Show("Phương trình vô nghiệm");
+            }
+        }
+
+        
+    }
+}
